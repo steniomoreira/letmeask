b9c73d5c56fa9295e7a057fd236cc5ce51dc4f14
Stenio Moreira <steniomoreira01@gmail.com>
2021-06-26 00:45:26 -0300

    Estipulado autorizaçao

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 import illustrationImg from './../assets/images/illustration.svg';
 import logoImg from './../assets/images/logo.svg';
@@ -10,43 +12,63 @@ import { Button } from '../components/Button';
 import './../styles/auth.scss';
 
 export function Home() {
-    const history = useHistory();
-    const { user, signInWithGoogle } = useAuth();
+	const history = useHistory();
+	const { user, signInWithGoogle } = useAuth();
+	const [roomCode, setRoomCode] = useState('');
 
-    async function handleCreateRoom() {
-        if (!user) {
-            await signInWithGoogle();
-        }
+	async function handleCreateRoom() {
+		if (!user) {
+			await signInWithGoogle();
+		}
 
-        history.push('/rooms/new');
-    }
+		history.push('/rooms/new');
+	}
 
-    return(
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-                <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire as dúvidas de sua audiência em tempo-real</p>
-            </aside>
-            <main>
-                <div className="main-content">
-                    <img src={logoImg} alt="Letmeask" />
-                    <button className="create-room" onClick={handleCreateRoom}>
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua salas com o Google
-                    </button>
-                    <div className="separator">Ou entre em uma sala</div>
-                    <form>
-                        <input 
-                            type="text"
-                            placeholder="Digite o código da sala"
-                        />
-                        <Button type="submit" >
-                            Entrar na sala
-                        </Button>
-                    </form>
-                </div>
-            </main>
-        </div>
-    );
+	async function handleJoinRoom(event: FormEvent) {
+		event.preventDefault();
+
+		if (roomCode.trim() === '') {
+			return;
+		}
+
+		const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+		if (!roomRef.exists()) {
+			alert('Room does not exists.');
+			return;
+		}
+
+		history.push(`/rooms/${roomCode}`);
+	}
+
+	return (
+		<div id="page-auth">
+			<aside>
+				<img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+				<strong>Crie salas de Q&amp;A ao-vivo</strong>
+				<p>Tire as dúvidas de sua audiência em tempo-real</p>
+			</aside>
+			<main>
+				<div className="main-content">
+					<img src={logoImg} alt="Letmeask" />
+					<button className="create-room" onClick={handleCreateRoom}>
+						<img src={googleIconImg} alt="Logo do Google" />
+						Crie sua salas com o Google
+					</button>
+					<div className="separator">Ou entre em uma sala</div>
+					<form onSubmit={handleJoinRoom}>
+						<input
+							type="text"
+							placeholder="Digite o código da sala"
+							onChange={event => setRoomCode(event.target.value)}
+							value={roomCode}
+						/>
+						<Button type="submit" >
+							Entrar na sala
+						</Button>
+					</form>
+				</div>
+			</main>
+		</div>
+	);
 }
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,5 +1,7 @@
-import { Link } from 'react-router-dom';
-// import { useAuth } from '../hooks/useAuth';
+import { FormEvent, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
 
 import illustrationImg from './../assets/images/illustration.svg';
 import logoImg from './../assets/images/logo.svg';
@@ -8,33 +10,54 @@ import { Button } from '../components/Button';
 import './../styles/auth.scss';
 
 export function NewRoom() {
-    // const { user } = useAuth();
+  const { user } = useAuth();
+  const history = useHistory();
+  const [newRoom, setNewRoom] = useState('');
 
-    return(
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-                <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire as dúvidas de sua audiência em tempo-real</p>
-            </aside>
-            <main>            
-                <div className="main-content">
-                    <img src={logoImg} alt="Letmeask" />
-                    <h2>Criar uma nova sala</h2>
-                    <form>
-                        <input 
-                            type="text"
-                            placeholder="Nome da sala"
-                        />
-                        <Button type="submit" >
-                            Criar sala
-                        </Button>
-                    </form>
-                    <p>
-                        Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
-                    </p>
-                </div>
-            </main>
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+    
+    if (newRoom.trim() === '') {
+      return;
+    }
+
+    const roomRef = database.ref('rooms');
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+
+    history.push(`/rooms/${firebaseRoom.key}`);
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas de sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              onChange={event => setNewRoom(event.target.value)}
+              value={newRoom}
+            />
+            <Button type="submit" >
+              Criar sala
+            </Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
+          </p>
         </div>
-    );
+      </main>
+    </div>
+  );
 }
