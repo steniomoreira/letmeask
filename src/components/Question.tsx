78a77ba0b10abae568a6d25efea5fb7faaa4ac33
Stenio Moreira <steniomoreira01@gmail.com>
2021-06-30 23:51:03 -0300

    Adicionado destaque na pergunta e marcar como respondida

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import '../styles/question.scss';
+import cx from 'classnames'; 
 
 type QuestionProps = {
     content: string;
@@ -7,12 +8,26 @@ type QuestionProps = {
         name: string,
         avatar: string
     };
+    isAnswered?: boolean;
+    isHighlighted?: boolean;
     children?: ReactNode;
 }
 
-export function Question({content, author, children}: QuestionProps) {
+export function Question({
+    content,
+    author,
+    isAnswered = false,
+    isHighlighted = false, 
+    children,
+}: QuestionProps) {
     return(
-        <div className="question">
+        <div 
+            className={cx(
+                'question',
+                { answered : isAnswered },
+                { highlighted : isHighlighted && !isAnswered }
+            )
+        }>
             <p>{content}</p>
             <footer>
                 <div className="user-info">
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -8,6 +8,8 @@ import { useRoom } from '../hooks/useRoom';
 import { database } from '../services/firebase';
 
 import logoImg from '../assets/images/logo.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
 import deleteImg from '../assets/images/delete.svg';
 
 import '../styles/room.scss';
@@ -37,6 +39,18 @@ export function AdminRoom() {
 		}
 	}
 
+	async function handleCheckQuestionAsAnswered(questionId: string) {
+		await database.ref(`/rooms/${roomId}/questions/${questionId}`).update({
+			isAnswered: true
+		});
+	}
+
+	async function handleHighlightQuestion(questionId: string) {
+		await database.ref(`/rooms/${roomId}/questions/${questionId}`).update({
+			isHighlighted: true
+		});
+	}
+
 	return (
 		<div id="page-room">
 			<header>
@@ -63,7 +77,25 @@ export function AdminRoom() {
 								key={question.id}
 								content={question.content}
 								author={question.author}
+								isAnswered={question.isAnswered}
+								isHighlighted={question.isHighlighted}
 							>
+								{!question.isAnswered && (
+									<>
+										<button
+											type="button"
+											onClick={() => handleCheckQuestionAsAnswered(question.id)}
+										>
+											<img src={checkImg} alt="Marcar pergunta como respondida" />
+										</button>
+										<button
+											type="button"
+											onClick={() => handleHighlightQuestion(question.id)}
+										>
+											<img src={answerImg} alt="Dar destaque à pergunta" />
+										</button>
+									</>
+								)}
 								<button
 									type="button"
 									onClick={() => handleDeleteQuestion(question.id)}
