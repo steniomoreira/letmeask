f90c3bdfdeec23d35ffb6aa69d4a94a9beb26b8c
Stenio Moreira <steniomoreira01@gmail.com>
2021-07-11 01:17:48 -0300

    Adicionado arquivos de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import { ThemeProvider } from 'styled-components';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-import { Home } from "./pages/Home";
-import { NewRoom } from "./pages/NewRoom";
-import { Room } from "./pages/Room";
-import { AdminRoom } from "./pages/AdminRoom";
 
 import { AuthContextProvider } from './contexts/AuthContext';
-import GlobalStyle from './styles/global';
 import { useTheme } from "./hooks/useTheme";
+import Routes from './routes';
+
+import GlobalStyle from './styles/global';
 
 function App() {
   const {theme} = useTheme();
@@ -16,16 +12,9 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <BrowserRouter>
-        <AuthContextProvider>
-          <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/rooms/new' component={NewRoom} />
-            <Route path='/rooms/:id' component={Room} />
-            <Route path='/admin/rooms/:id' component={AdminRoom} />
-          </Switch>
-        </AuthContextProvider>  
-      </BrowserRouter>
+      <AuthContextProvider>
+      <Routes />
+      </AuthContextProvider>  
     </ThemeProvider>
   );
 }
---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' exact component={Home} />
+                <Route path='/rooms/new' component={NewRoom} />
+                <Route path='/rooms/:id' component={Room} />
+                <Route path='/admin/rooms/:id' component={AdminRoom} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
